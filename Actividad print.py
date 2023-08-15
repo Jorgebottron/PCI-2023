33081645f53b5dee1521c7ff64a665a0ef2ff7f0
Jorgebottron <142025516+Jorgebottron@users.noreply.github.com>
2023-08-15 16:37:48 -0600

    Add files via upload

---
+++ Actividad print.py
@@ -0,0 +1,6 @@
+#Aquí se imprime texto
+print ("Tecnologico de Monterrey")
+print ("Bienvenido al curso de Pensamiento Computacional para Ingeniería")
+print ("Atentamente: Los Profesores")
+
+print ("Tecnológico de Monterrey\nBienvenido al curso de Pensamiento Computacional para la Ingeniería\n\tAtentamente: Los profesores")
