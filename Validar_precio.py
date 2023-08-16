9a814048f44144b9dbd0d226230e1a54ec22fdda
Jorgebottron <142025516+Jorgebottron@users.noreply.github.com>
2023-08-15 21:14:13 -0600

    Add files via upload

---
+++ Validar_precio.py
@@ -0,0 +1,10 @@
+precio = float(input("Introdice el precio: "))
+mensaje = ""
+if precio > 0 :
+  #9  print("El precio es válido")
+    mensaje = "El precio es válido"
+else :
+ #   print("El precio no es válido")
+    mensaje = "El precio es válido"
+print(mensaje)
+# print(f"Precio: ${precio}")
