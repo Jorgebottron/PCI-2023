ef5803330b6fa99534372cc80064ae7170470b65
Jorgebottron <142025516+Jorgebottron@users.noreply.github.com>
2023-09-01 21:15:53 -0600

    Avance1.py
    Este es el avance 3 que ahora posee funciones. Ahora todo el código está dentro de una clase para poder llamarlo de una manera más simple y rápido. Más aparte, junté todo el código en una función main para tener lo principal, y coloqué otra función para definir y realizar la función del IVA. Espero que sea suficiente. Ya después veré como cambiarle el nombre a uno más creativo. Feliz fin de semana.

--- Avance1.py
+++ Avance1.py
@@ -1,38 +1,58 @@
-#Aquí recibo al usuario y le pregunto su nombre
-Usuario = (input("Inserta tu nombre: "))
-print ("Bienvenido " + Usuario)
+class Store:
+    def __init__ (self):
+        #Aquí recibo al usuario y le pregunto su nombre
+        Usuario = (input("Inserta tu nombre: "))
+        print ("Bienvenido " + Usuario)
 
-#Aquí pregunto por lo que quiere comprar
-total = 0
-costo = 0
-print ("Este es el menú de productos. Elige el número del producto que deseas comprar:")
-print("1: Pantalon ")
-print("2: Playera")
-print("3: Chaqueta")
-print("4: Calcetines")
+        #Aquí pregunto por lo que quiere comprar
+        total = 0
+        costo = 0
+        iva = 0.0
+        totalIva = 0.0
+        print ("Este es el menú de productos. Elige el número del producto que deseas comprar:")
+        print("1: Pantalon ")
+        print("2: Playera")
+        print("3: Chaqueta")
+        print("4: Calcetines")
 
-n = (input("¿Quieres hacer una compra (si/no)? "))
+        #Aquí hago el ciclo que calcula el precio 
+        n = (input("¿Quieres hacer una compra (si/no)? "))
 
-while n == "si" :    
-        opcion = int(input("Ingresa el número del producto(1-4): "))            
+        while n == "si" :    
+            opcion = int(input("Ingresa el número del producto(1-4): "))            
         
-        if opcion == 1:
-            costo = 100                
-        elif opcion == 2:
-            costo = 125
-        elif opcion == 3:
-            costo = 300
-        elif opcion == 4:           
-            costo = 35
-                       
-        total = total + costo
+            if opcion == 1:
+                costo = 100
+            elif opcion == 2:
+                costo = 125
+            elif opcion == 3:
+                costo = 300
+            elif opcion == 4:           
+                costo = 35
+
+            iva = self.__getIva(costo) 
+            
+            total = total + ( costo + iva )
+            totalIva = totalIva + iva
+            
+            print("El costo de tu producto es: $" + str(costo))
+            print ("Y el iva del producto es: " + str(iva))
         
-        print("El total de tu compra es: " + str(total))
-        
-        n = (input("¿Quieres hacer otra compra (si/no)? "))
-        
-        
-  
-print ("El total de tu compra es de " + str(total))
-print ("Gracias por tu compra")
-  
+            n = (input("¿Quieres hacer otra compra (si/no)? "))
+            
+            
+            
+        print ("El total de tu compra fue de $" + str(total))
+        print ("The total cost of IVA is: " + str(totalIva))
+        print ("Gracias por tu compra")
+    
+    #Aqui defino la función iva 
+    #costo_iva significa el producto con iva
+    def getIva(self, value):        
+      return (value * 16) /100
+
+
+    __getIva = getIva
+    
+    
+Store()
